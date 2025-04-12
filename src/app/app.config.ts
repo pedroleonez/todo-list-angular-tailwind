6dcf648ac2694b624b030985a45f2dc9ad4cfcec
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-12 19:05:54 -0300

    chore: add providers and github settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,20 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "todo-list-e0082", appId: "1:304269247917:web:0a5386144f701352fe8d24", storageBucket: "todo-list-e0082.firebasestorage.app", apiKey: "AIzaSyCOobhGckum3vjD_cHaw-rr-y4nZxfpYCo", authDomain: "todo-list-e0082.firebaseapp.com", messagingSenderId: "304269247917" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'todo-list-e0082',
+        appId: '1:304269247917:web:0a5386144f701352fe8d24',
+        storageBucket: 'todo-list-e0082.firebasestorage.app',
+        apiKey: 'AIzaSyCOobhGckum3vjD_cHaw-rr-y4nZxfpYCo',
+        authDomain: 'todo-list-e0082.firebaseapp.com',
+        messagingSenderId: '304269247917',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
