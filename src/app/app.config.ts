9e3da16688bd1abdc5eca90031afafea1cb59020
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-12 14:04:45 -0300

    feat: add tailwind and firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "todo-list-e0082", appId: "1:304269247917:web:0a5386144f701352fe8d24", storageBucket: "todo-list-e0082.firebasestorage.app", apiKey: "AIzaSyCOobhGckum3vjD_cHaw-rr-y4nZxfpYCo", authDomain: "todo-list-e0082.firebaseapp.com", messagingSenderId: "304269247917" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
