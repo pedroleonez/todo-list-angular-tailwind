cc463048a5bfb71a221fc6aaa2b33258a6657501
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-13 14:01:13 -0300

    feat: add validators and onSubmit test

--- src/app/auth/features/sign-up/sign-up.component.ts
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -1,30 +1,60 @@
 import { Component, inject } from '@angular/core';
 import {
-  FormBuilder,
   FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-
-interface FormSignUp {
-  email: FormControl<string | null>;
-  password: FormControl<string | null>;
-}
+import { CommonModule } from '@angular/common';
+import { FormFieldName, getErrorMessage } from '../../utils/validators';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
 })
 export default class SignUpComponent {
-  private readonly _formBuilder = inject(FormBuilder);
+  private readonly formBuilder = inject(NonNullableFormBuilder);
 
-  form = this._formBuilder.group<FormSignUp>({
-    email: this._formBuilder.control('', [
-      Validators.required,
-      Validators.email,
-    ]),
-    password: this._formBuilder.control('', [Validators.required]),
-  });
+  form: FormGroup<{
+    id: FormControl<number>;
+    name: FormControl<string>;
+    email: FormControl<string>;
+    password: FormControl<string>;
+  }>;
+
+  constructor() {
+    this.form = this.formBuilder.group({
+      id: null as unknown as number,
+      name: ['', [Validators.required, Validators.maxLength(100)]],
+      email: [
+        '',
+        [Validators.required, Validators.email, Validators.maxLength(100)],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(100),
+        ],
+      ],
+    });
+  }
+
+  errorMessage(fieldName: FormFieldName): string | null {
+    return getErrorMessage(fieldName, this.form);
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    const { email, password } = this.form.value;
+
+    if (!email || !password) return;
+
+    console.log({ email, password });
+  }
 }
