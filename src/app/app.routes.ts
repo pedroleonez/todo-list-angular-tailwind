bfe6e04c030aff74e93b251fa4f067a1b88d36b3
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-13 16:16:58 -0300

    feat: add path tasks and null

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,12 @@ export const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/features/auth.routes'),
   },
-  // {
-  //   path: 'tasks',
-  // },
+  {
+    path: 'tasks',
+    loadChildren: () => import('./task/features/tasks.routes'),
+  },
+  {
+    path: '**',
+    redirectTo: '/tasks',
+  }
 ];
