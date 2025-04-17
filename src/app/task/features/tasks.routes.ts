081ab65becb1e720a0843068eaa07772fcc2c8a1
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-17 16:30:57 -0300

    feat: add routes of new and edit

--- src/app/task/features/tasks.routes.ts
+++ src/app/task/features/tasks.routes.ts
@@ -3,6 +3,14 @@ import { Routes } from "@angular/router";
 export default [
   {
     path: '',
-    loadComponent: () => import('./task-list/task-list.component').then(m => m.TaskListComponent),
+    loadComponent: () => import('./task-list/task-list.component').then(m => m.default),
   },
+  {
+    path: 'new',
+    loadComponent: () => import('./task-form/task-form.component').then(m => m.default),
+  },
+  {
+    path: 'edit/:id',
+    loadComponent: () => import('./task-form/task-form.component').then(m => m.default),
+  }
 ] as Routes;
