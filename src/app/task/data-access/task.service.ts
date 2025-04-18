d6c217ad44400d23382bd64c5815ef53347ef099
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-18 15:31:22 -0300

    feat: update getTasks and add create, update and delete

--- src/app/task/data-access/task.service.ts
+++ src/app/task/data-access/task.service.ts
@@ -4,9 +4,16 @@ import {
   addDoc,
   collection,
   collectionData,
+  deleteDoc,
+  doc,
   Firestore,
+  getDoc,
+  query,
+  updateDoc,
+  where,
 } from '@angular/fire/firestore';
 import { catchError, Observable, tap, throwError } from 'rxjs';
+import { AuthStateService } from '../../shared/data-access/auth-state.service';
 
 export interface Task {
   id: string;
@@ -18,31 +25,62 @@ export type TaskCreate = Omit<Task, 'id'>;
 
 const PATH = 'tasks';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class TaskService {
   private readonly _firestore = inject(Firestore);
+  private readonly _authState = inject(AuthStateService);
 
   private readonly _collection = collection(this._firestore, PATH);
+  private readonly _query = query(
+    this._collection,
+    where('userId', '==', this._authState.currentUser?.uid)
+  )
 
   loading = signal<boolean>(true);
 
-  getTasks = toSignal((collectionData(this._collection, {idField: 'id'}) as Observable<Task[]>).pipe(
-    tap(() => {
-      this.loading.set(false);
-    }),
-    catchError(error => {
-      this.loading.set(false);
-      return throwError(() => error);
-    })
-  ), {
-    initialValue: [],
-  });
+  getTasks = toSignal(
+    (
+      collectionData(this._query, { idField: 'id' }) as Observable<Task[]>
+    ).pipe(
+      tap(() => {
+        this.loading.set(false);
+      }),
+      catchError((error) => {
+        this.loading.set(false);
+        return throwError(() => error);
+      })
+    ),
+    {
+      initialValue: [],
+    }
+  );
 
-  create(task: TaskCreate) {
-    return addDoc(this._collection, task);
+  constructor() {
+    console.log(this._authState.currentUser);
   }
 
-  constructor() {}
+  getTask(id: string) {
+    const docRef = doc(this._collection, id);
+    return getDoc(docRef);
+  }
+
+  create(task: TaskCreate) {
+    return addDoc(this._collection, {
+      ...task,
+      userId: this._authState.currentUser?.uid,
+    });
+  }
+
+  update(id: string, task: TaskCreate) {
+    const docRef = doc(this._collection, id);
+    return updateDoc(docRef, {
+      ...task,
+      userId: this._authState.currentUser?.uid,
+    });
+  }
+
+    delete(id: string) {
+    const docRef = doc(this._collection, id);
+    return deleteDoc(docRef);
+  }
 }
