9e5615a932472a76e1fd918e699513ff660ba4a6
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-18 15:28:06 -0300

    feat: add delete method

--- src/app/task/features/task-list/task-list.component.ts
+++ src/app/task/features/task-list/task-list.component.ts
@@ -2,14 +2,26 @@ import { Component, inject } from '@angular/core';
 import { TableComponent } from '../../../shared/ui/table/table.component';
 import { RouterLink } from '@angular/router';
 import { TaskService } from '../../data-access/task.service';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-task-list',
   imports: [TableComponent, RouterLink],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrl: './task-list.component.scss',
+  providers: [TaskService],
 })
 export default class TaskListComponent {
   taskService = inject(TaskService);
+
+  async deleteTask(id: string) {
+    try {
+      await this.taskService.delete(id);
+      toast.success('Tarefa excluída com sucesso!');
+    } catch (error) {
+      toast.error('Erro ao excluir a tarefa.');
+      console.error(error);
+    }
+  }
 
 }
