0f5d36ca3c6026b1e388a3a31e0c705ecc431209
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-17 22:03:27 -0300

    feat: add input required Task

--- src/app/shared/ui/table/table.component.ts
+++ src/app/shared/ui/table/table.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, effect, input } from '@angular/core';
+import { Task } from '../../../task/data-access/task.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  tasks = input.required<Task[]>();
 
 }
