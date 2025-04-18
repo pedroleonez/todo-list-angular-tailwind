79765c3e00d770662d0c93d97f9afdaefc54b155
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-18 15:30:37 -0300

    feat: add output and deleteTask

--- src/app/shared/ui/table/table.component.ts
+++ src/app/shared/ui/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Task } from '../../../task/data-access/task.service';
 import { RouterLink } from '@angular/router';
 
@@ -10,5 +10,12 @@ import { RouterLink } from '@angular/router';
 })
 export class TableComponent {
   tasks = input.required<Task[]>();
+  onDelete = output<string>();
+
+  deleteTask(id: string) {
+    if (confirm('Tem certeza que deseja excluir esta tarefa?')) {
+      this.onDelete.emit(id);
+    }
+  }
 
 }
