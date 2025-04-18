323f56eb3fc41f21b3949b3977e65fca9224007b
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-17 22:03:08 -0300

    feat: impl task service

--- src/app/task/data-access/task.service.ts
+++ src/app/task/data-access/task.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  Firestore,
+} from '@angular/fire/firestore';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+
+export interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+export type TaskCreate = Omit<Task, 'id'>;
+
+const PATH = 'tasks';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private readonly _firestore = inject(Firestore);
 
-  constructor() { }
+  private readonly _collection = collection(this._firestore, PATH);
+
+  loading = signal<boolean>(true);
+
+  getTasks = toSignal((collectionData(this._collection, {idField: 'id'}) as Observable<Task[]>).pipe(
+    tap(() => {
+      this.loading.set(false);
+    }),
+    catchError(error => {
+      this.loading.set(false);
+      return throwError(() => error);
+    })
+  ), {
+    initialValue: [],
+  });
+
+  create(task: TaskCreate) {
+    return addDoc(this._collection, task);
+  }
+
+  constructor() {}
 }
